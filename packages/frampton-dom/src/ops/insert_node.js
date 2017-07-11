fd8bb79db3b6bb5c41f837bc59e1205b02c8dc44
Kevin Greene <kgreene@linkedin.com>
2017-07-11 11:52:53 -0700

    Adding example usage

--- packages/frampton-dom/src/ops/insert_node.js
+++ packages/frampton-dom/src/ops/insert_node.js
@@ -11,10 +11,13 @@ import transitionIn from 'frampton-dom/utils/transition_in';
  * @param {VirtualNode} vnode
  */
 export default function insert_node(parent, current, vnode, messages) {
+
   const child = createElement(vnode, messages);
+
   if (vnode.attributes.transitionIn) {
     transitionIn(child, vnode.attributes.transitionIn);
   }
+
   if (parent) {
     if (current) {
       parent.insertBefore(child, current);
